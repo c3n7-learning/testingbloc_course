1238cf28d3fbc8fb2fcdb82c76941f8defbbb80d
Timothy Karani <kartimothy@gmail.com>
2023-11-05 22:15:03 +0300

    step 4

---
+++ testingbloc_course/lib/dialogs/loading_screen.dart
@@ -0,0 +1,110 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:testingbloc_course/dialogs/loading_screen_controller.dart';
+
+class LoadingScreen {
+  // Singleton pattern
+  LoadingScreen._sharedInstance();
+  static final LoadingScreen _shared = LoadingScreen._sharedInstance();
+  factory LoadingScreen.instance() => _shared;
+
+  LoadingScreenController? _controller;
+
+  void show({
+    required BuildContext context,
+    required String text,
+  }) {
+    if (_controller?.update(text) ?? false) {
+      return;
+    }
+    _controller = _showOverlay(
+      context: context,
+      text: text,
+    );
+  }
+
+  void hide() {
+    _controller?.close();
+    _controller = null;
+  }
+
+  LoadingScreenController _showOverlay({
+    required BuildContext context,
+    required String text,
+  }) {
+    final textStream = StreamController<String>();
+    textStream.add(text);
+
+    final state = Overlay.of(context);
+    final renderBox = context.findRenderObject() as RenderBox;
+    final size = renderBox.size;
+
+    final overlay = OverlayEntry(
+      builder: (context) {
+        return Material(
+          color: Colors.black.withAlpha(150),
+          child: Center(
+            child: Container(
+              constraints: BoxConstraints(
+                maxHeight: size.height * 0.8,
+                maxWidth: size.width * 0.8,
+                minWidth: size.width * 0.5,
+              ),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: SingleChildScrollView(
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      const CircularProgressIndicator(),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      StreamBuilder<String>(
+                        stream: textStream.stream,
+                        builder: (context, snapshot) {
+                          if (snapshot.hasData) {
+                            return Text(
+                              snapshot.data!,
+                              textAlign: TextAlign.center,
+                            );
+                          }
+                          return Container();
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+        );
+      },
+    );
+
+    state.insert(overlay);
+
+    return LoadingScreenController(
+      close: () {
+        textStream.close(); // close the stream controller
+
+        // Remove the overlay from the screen
+        overlay.remove();
+        return true;
+      },
+      update: (text) {
+        textStream.add(text);
+        return true;
+      },
+    );
+  }
+}
--- testingbloc_course/lib/main.dart
+++ testingbloc_course/lib/main.dart
@@ -2,6 +2,19 @@ import 'package:flutter/material.dart';
 
 import 'dart:developer' as devtools show log;
 
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:testingbloc_course/apis/login_api.dart';
+import 'package:testingbloc_course/apis/notes_api.dart';
+import 'package:testingbloc_course/bloc/actions.dart';
+import 'package:testingbloc_course/bloc/app_bloc.dart';
+import 'package:testingbloc_course/bloc/app_state.dart';
+import 'package:testingbloc_course/dialogs/generic_dialog.dart';
+import 'package:testingbloc_course/dialogs/loading_screen.dart';
+import 'package:testingbloc_course/models.dart';
+import 'package:testingbloc_course/strings.dart';
+import 'package:testingbloc_course/views/iterable_list_view.dart';
+import 'package:testingbloc_course/views/login_view.dart';
+
 extension Log on Object {
   void log() => devtools.log(toString());
 }
@@ -24,11 +37,64 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Title'),
+    return BlocProvider(
+      create: (context) => AppBloc(
+        loginApi: LoginApi(),
+        notesApi: NotesApi(),
       ),
-      body: const SizedBox(),
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text(homePage),
+        ),
+        body: BlocConsumer<AppBloc, AppState>(
+          listener: (context, state) {
+            // take care of loading screen
+            if (state.isLoading) {
+              LoadingScreen.instance().show(
+                context: context,
+                text: pleaseWait,
+              );
+            } else {
+              LoadingScreen.instance().hide();
+            }
+
+            // display possible errors
+            if (state.loginError != null) {
+              showGenericDialog(
+                context: context,
+                title: loginErrorDialogTitle,
+                content: loginErrorDialogContent,
+                optionsBuilder: () => {ok: true},
+              );
+            }
+
+            // If we are logged in but have no fetched notes
+            if (!state.isLoading &&
+                state.loginError == null &&
+                state.loginHandle == const LoginHandle.fooBar() &&
+                state.fetchedNotes == null) {
+              context.read<AppBloc>().add(const LoadNotesAction());
+            }
+          },
+          builder: (context, state) {
+            final notes = state.fetchedNotes;
+            if (notes == null) {
+              return LoginView(
+                onLoginTapped: (email, password) {
+                  context.read<AppBloc>().add(
+                        LoginAction(
+                          email: email,
+                          password: password,
+                        ),
+                      );
+                },
+              );
+            } else {
+              return notes.toListView();
+            }
+          },
+        ),
+      ),
     );
   }
 }
